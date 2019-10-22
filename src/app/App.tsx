98632d6a8f85ca5e3719367ae158c51a01a40724
Enrique Martín García <enriquem.developer@gmail.com>
2019-10-22 21:54:20 +0200

    inital commit

---
+++ src/app/App.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const App: React.FC = () => {
+  return <div>Hello World!</div>;
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./app/App";
+
+const appContainer = document.getElementById("root");
+ReactDOM.render(<App />, appContainer);
