d21c8048bd0ae51959eddae7fc2bcead6227d491
Simone Ghergo <simone.ghergo.external@ynap.com>
2020-06-30 19:03:41 +0200

    Removed comma

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 import { connect } from 'react-redux';
 import { uploadFile } from './actions/fileActions';
-import { encryptFile,  } from './actions/encryptActions';
+import { encryptFile } from './actions/encryptActions';
 import { FileDataInterface } from './interfaces';
 import { EncryptedFileInterface } from './interfaces';
 
